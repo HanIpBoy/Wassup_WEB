55a62765fb98d1702f8579a0b2e175a3beec7068
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-04-18 19:56:32 +0900

    .gitignore, prettier 설치

--- calendar/src/components/Calendar/Calendar.jsx
+++ calendar/src/components/Calendar/Calendar.jsx
@@ -12,27 +12,27 @@ import CalendarModal from '../Modals/CalendarModal';
 const RenderHeader = ({ currentMonth, prevMonth, nextMonth }) => {
   return (
     <>
-    <img className={styles.mainIcon} src={MainIcon} />
-    <div className={styles.header + ' row'}>
-    
-      <div className={styles['col-start']}>
-      <Icon icon="bi:arrow-left-circle-fill" onClick={prevMonth} />
-        <span className={styles.text}>
-            {format(currentMonth, 'yyyy')}년 
-          <span className={`${styles.text} ${styles.month}`}>
-            {format(currentMonth, 'M')}월
+      <img className={styles.mainIcon} src={MainIcon} />
+      <div className={styles.header + ' row'}>
+
+        <div className={styles['col-start']}>
+          <Icon icon="bi:arrow-left-circle-fill" onClick={prevMonth} />
+          <span className={styles.text}>
+            {format(currentMonth, 'yyyy')}년
+            <span className={`${styles.text} ${styles.month}`}>
+              {format(currentMonth, 'M')}월
+            </span>
+            <Icon icon="bi:arrow-right-circle-fill" onClick={nextMonth} />
           </span>
-          <Icon icon="bi:arrow-right-circle-fill" onClick={nextMonth} />
-        </span>
-        
-      </div>
-      {/* <div className={styles['col-end']}>
+
+        </div>
+        {/* <div className={styles['col-end']}>
         <Icon icon="bi:arrow-left-circle-fill" onClick={prevMonth} />
         <Icon icon="bi:arrow-right-circle-fill" onClick={nextMonth} />
       </div> */}
-    </div>
+      </div>
     </>
-    
+
   );
 };
 
@@ -64,19 +64,18 @@ const RenderCells = ({ currentMonth, selectedDate, onDateClick }) => {
       const cloneDay = day;
       days.push(
         <div
-          className={`${styles.col} ${styles.cell} ${
-            !isSameMonth(day, monthStart)
+          className={`${styles.col} ${styles.cell} ${!isSameMonth(day, monthStart)
               ? styles.disabled
               : isSameDay(day, selectedDate)
-              ? styles.selected
-              : format(currentMonth, 'M') !== format(day, 'M')
-              ? styles['not-valid']
-              : styles.valid
-          }`}
+                ? styles.selected
+                : format(currentMonth, 'M') !== format(day, 'M')
+                  ? styles['not-valid']
+                  : styles.valid
+            }`}
           key={day}
           onClick={() => CalendarModalOn(true)}
         >
-            {/* onDateClick(parse(cloneDay)) -> */}
+          {/* onDateClick(parse(cloneDay)) -> */}
           <span
             className={
               format(currentMonth, 'M') !== format(day, 'M') ? `${styles.text} ${styles['not-valid']}` : styles.text
@@ -114,22 +113,22 @@ export default function Calendar() {
 
   const [CalendarModalOn, setCalendarModalOn] = useState(false);
 
-  return(
+  return (
     <>
-    <CalendarModal show={CalendarModalOn} onHide={()=>CalendarModalOn(false)} />
-    <div className={styles.calendar}>
+      <CalendarModal show={CalendarModalOn} onHide={() => CalendarModalOn(false)} />
+      <div className={styles.calendar}>
         <RenderHeader
-            currentMonth={currentMonth}
-            prevMonth={prevMonth}
-            nextMonth={nextMonth}
+          currentMonth={currentMonth}
+          prevMonth={prevMonth}
+          nextMonth={nextMonth}
         />
         <RenderDays />
         <RenderCells
-            currentMonth={currentMonth}
-            selectedDate={selectedDate}
-            onDateClick={onDateClick}
+          currentMonth={currentMonth}
+          selectedDate={selectedDate}
+          onDateClick={onDateClick}
         />
-    </div>
+      </div>
     </>
   );
 };
