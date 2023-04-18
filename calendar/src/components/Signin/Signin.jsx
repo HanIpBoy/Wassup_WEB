45622e2d556571e7b6e332ed9098db2fb011f1ea
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-04-18 21:54:02 +0900

    signin 수정

--- calendar/src/components/Signin/Signin.jsx
+++ calendar/src/components/Signin/Signin.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -10,82 +10,100 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import MainIcon from '../../images/MainIcon.png';
 import styles from './Signin.module.css';
+import axios from 'axios'
 
-
-const theme = createTheme();
 
 export default function SignIn() {
 
-const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-    id: data.get('id'),
-    password: data.get('password'),
-    });
-};
+    const [emailInput, setEmailInput] = useState('')
+    const [passwordInput, setPasswordInput] = useState('')
+    const [checkEmail, setCheckEmail] = useState(false)
 
-return (
-    <ThemeProvider theme={theme}>
-    <Container component="main" maxWidth="xs">
+    const handleSubmit = async (event) => { // await을 사용하기 위해 async 함수로 선언
+        event.preventDefault();
+        const response = await axios.post('http://localhost:8080/auth/signin', { // axios는 항상 await과 함께 사용
+            userId: emailInput,
+            password: passwordInput
+        })
 
-        <CssBaseline />
-        <Box
-        sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        }}
-        >
+        const { status, data } = response;
+    };
 
-        <img className={styles.mainIcon} src={MainIcon} />
+    const handleInputEmail = (event) => {
+        const { value } = event.target
+        setEmailInput(value)
+    }
 
-        <div className={styles.container}>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField className={styles.textField}
-            margin="normal"
-            required
-            fullWidth
-            id="id"
-            label="이메일"
-            name="id"
-            autoComplete="email"
-            autoFocus
-            />
-            <TextField className={styles.textField}
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="비밀번호"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            />
-            <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="아이디 기억하기"
-            />
-            <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ backgroundColor:'#0040ff', mt: 3, mb: 2 }}
+    const handleInputPassword = (event) => {
+        const { value } = event.target
+        setPasswordInput(value)
+    }
+
+    const handleClickCheckEmail = (event) => {
+        console.log(event)
+        const { value, checked } = event.target
+        setCheckEmail(checked)
+    }
+
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
             >
-            로그인
-            </Button>
-            <button className={styles.signUp} href="#">계정을 만들어보자!</button>
-        </Box>
-        <Link className={styles.forgotPassword} href="#" variant="body1">
-            비밀번호를 까먹으셨나?
-        </Link>
-        </div>
-        </Box>
-    </Container>
-    </ThemeProvider>
-);
+                <img className={styles.mainIcon} src={MainIcon} />
+
+                <div className={styles.container}>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField className={styles.textField}
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="id"
+                            label="이메일"
+                            name="id"
+                            autoComplete="email"
+                            autoFocus
+                            onInput={handleInputEmail}
+                        />
+                        <TextField className={styles.textField}
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="비밀번호"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onInput={handleInputPassword}
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value={checkEmail} color="primary" onClick={handleClickCheckEmail} />}
+                            label="아이디 기억하기"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ backgroundColor: '#0040ff', mt: 3, mb: 2 }}
+                        >
+                            로그인
+                        </Button>
+                        <button className={styles.signUp} href="#">계정을 만들어보자!</button>
+                    </Box>
+                    <Link className={styles.forgotPassword} href="#" variant="body1">
+                        비밀번호를 까먹으셨나?
+                    </Link>
+                </div>
+            </Box>
+        </Container>
+    );
 }
