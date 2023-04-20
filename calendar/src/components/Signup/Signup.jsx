cd8aa73d61f1750b5b4734ba6d19f52e2f457d95
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-04-21 01:05:33 +0900

    signup페이지 주석처리

--- calendar/src/components/Signup/Signup.jsx
+++ calendar/src/components/Signup/Signup.jsx
@@ -16,29 +16,29 @@ import styles from './Signup.module.css';
 import axios from 'axios'
 
 export default function SignUp() {
-    const [nameInput, setNameInput] = useState('')
-    const [emailInput, setEmailInput] = useState('')
-    const [passwordInput, setPasswordInput] = useState('')
-    const [birthInput, setBirthInput] = useState(dayjs());
-    const [codeInput, setCodeInput] = useState('');
-    const [passwordCheck, setPasswordCheck] = useState(false);
+    const [nameInput, setNameInput] = useState('') //이름 값
+    const [emailInput, setEmailInput] = useState('') // 이메일 값
+    const [passwordInput, setPasswordInput] = useState('') //비밀번호 값
+    const [birthInput, setBirthInput] = useState(dayjs()); //생일 값
+    const [codeInput, setCodeInput] = useState(''); //이메일 인증번호 값
+    const [passwordCheck, setPasswordCheck] = useState(false); //비밀번호확인 값
 
-    const handleInputName = (event) => {
+    const handleInputName = (event) => { //이름 이벤트 핸들러
         const { value } = event.target
         setNameInput(value)
     }
 
-    const handleInputEmail = (event) => {
+    const handleInputEmail = (event) => { //이메일 이벤트 핸들러
         const { value } = event.target
         setEmailInput(value)
     }
 
-    const handleInputPassword = (event) => {
+    const handleInputPassword = (event) => { //비밀번호 이벤트 핸들러
         const { value } = event.target
         setPasswordInput(value)
     }
 
-    const handleInputPasswordCheck = (event) => {
+    const handleInputPasswordCheck = (event) => { //비밀번호 확인 이벤트 핸들러
         const { value } = event.target
         if (value !== passwordInput) {
             setPasswordCheck(true)
@@ -47,42 +47,43 @@ export default function SignUp() {
         }
     }
 
-    const handleInputVerifyEmailCode = (event) => {
+    const handleInputVerifyEmailCode = (event) => { //인증번호 입력 이벤트 핸들러
         const { value } = event.target
         setCodeInput(value)
     }
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = async (event) => { //회원가입 버튼 이벤트 핸들러
         event.preventDefault();
 
 
 
-        const responseEmail = await axios.post('http://13.125.238.167:8080/auth/email-verify', {
+        const responseEmail = await axios.post('http://13.125.122.132:8080/auth/email-verify', { //이메일 인증요청
             userId: emailInput,
             emailAuthCode: codeInput
         })
 
-        if (responseEmail.data.includes('success')) {
-            window.alert('이메일 인증에 성공했습니다!')
+        if (responseEmail.data.includes('success')) { //만약 서버에서 보내준 값에 "success" 값이 있을 경우
+            window.alert('이메일 인증에 성공했습니다!') //성공 alert를 띄운 후 그대로 실행
         } else {
-            window.alert('이메일 인증에 실패했습니다!')
+            window.alert('이메일 인증에 실패했습니다!') //실패 alert를 띄운 후 그대로 종료
             return
         }
 
-        let month = birthInput.$M + 1
-        let day = birthInput.$D
+        //데이터값들을 서버 요청 양식에 맞게 재가공
+        let month = birthInput.$M + 1 //월
+        let day = birthInput.$D //일
 
-        if (month < 10) {
+        if (month < 10) { //10월 이전 생월일 경우
             month = '0' + month
         }
 
-        if (day < 10) {
+        if (day < 10) { //10일 이전 생일일 경우
             day = '0' + birthInput.$D
         }
 
-        const birth = birthInput.$y + '-' + month + '-' + day
+        const birth = birthInput.$y + '-' + month + '-' + day //서버 요청 양식에 맞춘 새로운 birth값 생성
 
-        const response = await axios.post('http://13.125.238.167:8080/auth/signup', {
+        const response = await axios.post('http://13.125.122.132:8080/auth/signup', { //회원가입 버튼 클릭시 서버로 post
             username: nameInput,
             userId: emailInput,
             password: passwordInput,
@@ -93,11 +94,12 @@ export default function SignUp() {
 
         console.log(response)
 
-        window.history.pushState('', '', 'localhost:3000/calendar')
+        window.history.pushState('', '', 'localhost:3000/calendar') //calendar 페이지로 이동
     };
 
-    const handleClickEmail = async (event) => {
-        const response = await axios.post('http://13.125.238.167:8080/auth/email-send', {
+
+    const handleClickEmail = async (event) => {   //이메일 인증하기 버튼 이벤트 핸들러
+        const response = await axios.post('http://13.125.122.132:8080/auth/email-send', {
             userId: emailInput
         })
     }
