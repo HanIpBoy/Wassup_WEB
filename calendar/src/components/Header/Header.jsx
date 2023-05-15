67c0ffc4ed406ca2bf1e1612c3ee5b9f1fbb422f
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-05-15 14:30:17 +0900

    헤더 아이콘 클릭시 navigate, 아이콘 위치 조절

--- calendar/src/components/Header/Header.jsx
+++ calendar/src/components/Header/Header.jsx
@@ -5,8 +5,11 @@ import cookie from 'js-cookie'
 import { useNavigate } from "react-router-dom";
 import Calendar from "../../images/Calendar.png";
 import CalendarTab from "../../images/CalendarTab.png";
-import GroupIcon from '../../images/GroupIcon.png';
-import SettingIcon from '../../images/SettingIcon.png';
+import Group from '../../images/Group.png';
+import GroupTab from '../../images/GroupTab.png';
+import Setting from '../../images/Setting.png';
+import SettingTab from '../../images/SettingTab.png';
+import { Link } from "react-router-dom";
 
 export default function Header() {
     const [username, setUserName] = useState('')
@@ -27,21 +30,36 @@ export default function Header() {
         navigate('/signin')
     }
 
+    const handleClickCalendarIcon = (event) => {
+        navigate('/calendar')
+        setTab('/calendar')
+    }
+
+    const handleClickGroupIcon = (event) => {
+        navigate('/groups')
+        setTab('/groups')
+    }
+
+    const handleClickSettingIcon = (event) => {
+        navigate('/settings')
+        setTab('/settings')
+    }
+
     return (
 
-        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '25px', fontFamily: 'var(--font-PoorStory)', margin: '0 50px' }}>
-            <img src={MainIcon} style={{ width: '80px', height: '100px', marginTop: '10px' }} />
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '25px', fontFamily: 'var(--font-PoorStory)', margin: '25px 50px' }}>
+            <img src={MainIcon} style={{ width: '80px', height: '100px', margin: '0 auto', paddingLeft: '120px' }} />
 
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
                     <p>{username} 님</p>
                     <div style={{ borderLeft: "solid gray", height: '50%', left: '50%', marginLeft: '10px' }}></div>
                     <Button variant="text" onClick={handleClickLogout} sx={{ color: 'gray', fontFamily: 'var(--font-PoorStory)' }}>로그아웃</Button>
                 </div>
                 <div style={{ display: 'flex', columnGap: '10px' }}>
-                    <img src={tab === '/calendar' ? CalendarTab : Calendar} style={{ ...headerStyle }} />
-                    <img src={GroupIcon} style={{ ...headerStyle }} />
-                    <img src={SettingIcon} style={{ ...headerStyle }} />
+                    <img src={tab === '/calendar' ? CalendarTab : Calendar} style={{ ...headerStyle }} onClick={handleClickCalendarIcon} />
+                    <img src={tab === '/groups' ? GroupTab : Group} style={{ ...headerStyle }} onClick={handleClickGroupIcon} />
+                    <img src={tab === '/settings' ? SettingTab : Setting} style={{ ...headerStyle }} onClick={handleClickSettingIcon} />
                 </div>
             </div>
         </div>
