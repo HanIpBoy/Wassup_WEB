b8a12a12a56191c2f444091a70730ac201e73426
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-05-17 01:52:56 +0900

    baseURL 변경

--- calendar/src/axios.js
+++ calendar/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import cookie from "js-cookie";
 
 const instance = axios.create({ //동일 형태로 url 설정
-    baseURL: 'http://43.202.6.236:8080'
+    baseURL: 'http://3.38.101.226:8080'
 })
 
 //인터셉터
