6ea0ff9f42aee65753b1ca8ba30a9468cb57244f
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-05-19 04:04:41 +0900

    더미데이터

--- calendar/src/containers/CalendarContainer.jsx
+++ calendar/src/containers/CalendarContainer.jsx
@@ -2,46 +2,18 @@ import axios from "../axios";
 import Calendar from "../components/Calendar/Calendar.jsx";
 import { useEffect, useState } from "react";
 import Header from "../components/Header/Header.jsx";
-// const dummy = [ //더미 데이터, 서버 작동되면 삭제해도 됨
-//     {
-//         "originKey": " ", "name": "스케쥴1 ",
-//         "startAt": "2023-04-17T19:00", "endAt": "2023-04-20T20:00",
-//         "userId": "abc@naver.com",
-//         "memo": " !",
-//         "notification": 1,
-//         "allDayToggle": 1,
-//         "createdAt": "2023-03-17T19:11",
-//         "lastModifiedAt": "2023-03-17T19:11"
-//     },
-//     {
-//         "originKey": " ", "name": "스케쥴2 ",
-//         "startAt": "2023-04-17T19:00", "endAt": "2023-04-17T20:00",
-//         "userId": "abc@naver.com",
-//         "memo": " !",
-//         "notification": 1,
-//         "allDayToggle": 1,
-//         "createdAt": "2023-03-17T19:11",
-//         "lastModifiedAt": "2023-03-17T19:11"
-//     },
-//     {
-//         "originKey": " ", "name": "스케쥴3 ",
-//         "startAt": "2023-04-17T19:00", "endAt": "2023-04-20T20:00",
-//         "userId": "abc@naver.com",
-//         "memo": " !",
-//         "notification": 1,
-//         "allDayToggle": 1,
-//         "createdAt": "2023-03-17T19:11",
-//         "lastModifiedAt": "2023-03-17T19:11"
-//     },
-// ]
 
 function formatDate(datetime) {
-    const date = datetime.substring(0, 10)
-    const time = datetime.substring(10)
-    const formatTime = time.replace('-', 'T')
-    const result = date + formatTime
+    if (datetime) {
+        const date = datetime.substring(0, 10)
+        const time = datetime.substring(10)
+        const formatTime = time.replace('-', 'T')
+        const result = date + formatTime
+        return result
 
-    return result
+    }
+
+    return ""
 }
 
 export default function CalendarContainer() {
