db86af9f659a8478c728fa15eec25525dcb69b79
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-05-22 13:37:12 +0900

    ip 수정

--- calendar/src/axios.js
+++ calendar/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import cookie from "js-cookie";
 
 const instance = axios.create({ //동일 형태로 url 설정
-    baseURL: 'http://3.38.94.13:8080'
+    baseURL: 'http://3.38.104.222:8080'
 })
 
 //인터셉터
