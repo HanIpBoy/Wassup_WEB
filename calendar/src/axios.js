4899a4cd3d635698c3f4d9c49d265715a244ebcc
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-05-23 14:44:38 +0900

    baseURL 수정 + Bearer 토큰 로직 수정

--- calendar/src/axios.js
+++ calendar/src/axios.js
@@ -2,15 +2,25 @@ import axios from "axios"
 import cookie from "js-cookie";
 
 const instance = axios.create({ //동일 형태로 url 설정
-    baseURL: 'http://13.125.215.231:8080'
+    baseURL: 'http://13.125.226.121:8080'
 })
 
 //인터셉터
 instance.interceptors.request.use(function (config) {
     const token = cookie.get('token') //쿠키의 토큰값을 가져와서 header에 넣어줌
     console.log('token', token)
-    config.headers = {
-        Authorization: `Bearer ${token}` //Authorization에 token값 넣기
+
+    if (
+        config.url.endsWith('auth/signup') ||
+        config.url.endsWith('/auth/email-send') ||
+        config.url.endsWith('auth/email-verify')
+    ) {
+        console.log('No token required');
+    } else {
+        //나머지 URL에는 'Bearer' 토큰을 포함시킴
+        config.headers = {
+            Authorization: `Bearer ${token}` // Authorization에 token값 넣기
+        }
     }
 
     // console.log(config)
