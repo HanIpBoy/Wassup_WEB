58c639ea90020ddfeb228a6c10226d8d56459752
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-05-23 21:02:18 +0900

    알람 페이지 대략적인 ui만 구현

--- calendar/src/components/Alarm/Alarm.jsx
+++ calendar/src/components/Alarm/Alarm.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import Header from "../Header/Header";
+import AlarmItem from './AlarmItem.jsx';
 
 export default function Alarm() {
+
+    const [updatedAlarms, setUpdatedAlarms] = useState('') //업데이트된 알람 상태 저장
+
+    const handleClickYes = () => { //예 버튼 클릭시 작동되는 핸들러
+
+    }
+
+    const handleClickNo = () => { //아니오 버튼 클릭시 작동되는 핸들러
+
+    }
+
     return (
         <>
+
             <Header />
             <div style={{
                 backgroundColor: 'rgba(219,230,243,0.5)',
@@ -15,13 +29,13 @@ export default function Alarm() {
                 borderRadius: '30px',
                 boxShadow: '2px 2px 10px rgba(0,0,0,0.2)'
             }}>
-                알람입니다
-
-                알람입니다
-
-                알람입니다
-
-                알람입니다
+                {/* {updatedAlarms.map((value, idx) => {
+                    return <>
+                        <hr style={{ borderTop: '1px solid rgba(0,0,0,0.1)', width: '100%' }} />
+                        <AlarmItem leaderName={value} key={idx} onClickYes={handleClickYes} onClickNo={handleClickNo} />
+                    </>
+                })} */}
+                <AlarmItem />
             </div>
         </>
     )
