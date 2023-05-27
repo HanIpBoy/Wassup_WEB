cee5b81e3891d70b06f41d746608b7251b16fb14
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-05-28 00:01:56 +0900

    아바타 수정

--- calendar/src/components/Avatar.jsx
+++ calendar/src/components/Avatar.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Avatar({ backgroundColor, userName, userId, onClick }) {
+export default function Avatar({ backgroundColor, userName, userId, onClick, groupMembers }) {
+    const [groupMember, setGroupMember] = useState(false)
+
+    if (groupMember === true) {
+
+    }
+
     const handleClickAvartar = () => {
         onClick(userId)
     }
