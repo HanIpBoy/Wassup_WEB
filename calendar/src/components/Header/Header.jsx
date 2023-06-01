1684f0b53d8adb9e5a53586661f6ac365c9c1b46
Jeonghan Kim <91442091+watermelonswag@users.noreply.github.com>
2023-06-01 14:31:06 +0900

    헤더부분 레이아웃 수정

--- calendar/src/components/Header/Header.jsx
+++ calendar/src/components/Header/Header.jsx
@@ -5,10 +5,13 @@ import cookie from 'js-cookie'
 import { useNavigate } from "react-router-dom";
 import Calendar from "../../images/Calendar.png";
 import CalendarTab from "../../images/CalendarTab.png";
+import Write from "../../images/btn_pen.png";
+import WriteTab from "../../images/btn_pen_clicked.png";
 import Group from '../../images/Group.png';
 import GroupTab from '../../images/GroupTab.png';
 import Alirm from '../../images/Alirm.png';
 import AlirmTab from '../../images/AlirmTab.png';
+import { Alert } from "@mui/material";
 
 export default function Header() {
     const [username, setUserName] = useState('')
@@ -49,25 +52,27 @@ export default function Header() {
         setTab('/calendar')
     }
 
+
     return (
+        <>
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '25px', fontFamily: 'var(--font-PoorStory)', margin: '25px 50px' }}>
+                <img src={MainIcon} onClick={handleClickMainIcon} style={{ width: '80px', height: '100px', margin: '0 auto', marginLeft: '47%', cursor: 'pointer' }} />
 
-        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '25px', fontFamily: 'var(--font-PoorStory)', margin: '25px 50px' }}>
-            <img src={MainIcon} onClick={handleClickMainIcon} style={{ width: '80px', height: '100px', margin: '0 auto', marginLeft: '47%', cursor: 'pointer' }} />
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '20%' }}>
+                    <div style={{ display: 'flex', alignItems: 'center', fontSize: '14px' }}>
+                        <div>{username} 님</div>
+                        <div style={{ borderLeft: "solid gray", height: '50%', left: '50%', marginLeft: '10px' }}></div>
+                        <Button variant="text" onClick={handleClickLogout} sx={{ color: 'gray', fontFamily: 'var(--font-PoorStory)', paddingTop: '7px' }}>로그아웃</Button>
+                    </div>
+                    <div style={{ display: 'flex', columnGap: '10px' }}>
+                        <img src={tab === '/calendar' ? CalendarTab : Calendar} style={{ ...headerStyle }} onClick={handleClickCalendarIcon} />
+                        <img src={tab === '/groups' || tab.startsWith('/groups/') ? GroupTab : Group} style={{ ...headerStyle }} onClick={handleClickGroupIcon} />
+                        <img src={tab === '/alirms' ? AlirmTab : Alirm} style={{ ...headerStyle }} onClick={handleClickAlirmIcon} />
 
-            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '20%' }}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <p>{username} 님</p>
-                    <div style={{ borderLeft: "solid gray", height: '50%', left: '50%', marginLeft: '10px' }}></div>
-                    <Button variant="text" onClick={handleClickLogout} sx={{ color: 'gray', fontFamily: 'var(--font-PoorStory)' }}>로그아웃</Button>
-                </div>
-                <div style={{ display: 'flex', columnGap: '10px' }}>
-                    <img src={tab === '/calendar' ? CalendarTab : Calendar} style={{ ...headerStyle }} onClick={handleClickCalendarIcon} />
-                    <img src={tab === '/groups' || tab.startsWith('/groups/') ? GroupTab : Group} style={{ ...headerStyle }} onClick={handleClickGroupIcon} />
-                    <img src={tab === '/alirms' ? AlirmTab : Alirm} style={{ ...headerStyle }} onClick={handleClickAlirmIcon} />
-
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
